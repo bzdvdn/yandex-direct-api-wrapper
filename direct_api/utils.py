a76e612c5db2d84787e34d10cf18a6b14a15ecd7
Bogdan <zavodchikov.g08@gmail.com>
2019-12-08 11:07:27 +0300

    add adgroups, ads, adimages entitites method

--- direct_api/utils.py
+++ direct_api/utils.py
@@ -2,5 +2,5 @@ def convert(word):
     return ''.join(x.capitalize() or '_' for x in word.split('_'))
 
 
-def generate_select_criteria(fields: list, function_kwargs: dict) -> dict:
+def generate_params(fields: list, function_kwargs: dict) -> dict:
     return {convert(field): function_kwargs[field] for field in fields if function_kwargs.get(field)}
